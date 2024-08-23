9e1bfb395cbf7fe3d97386cdb2b3b77523c30660
banana0324 <david0324t@gmail.com>
2024-08-24 02:10:34 +0800

    drum

---
+++ WEB_learning/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,10 @@
+
+// document.querySelector(".set").addEventListener("click",function (){
+//     alert("I got click！")
+// });
+
+for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+        alert("I got click！")
+    });
+}
