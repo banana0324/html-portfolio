2a8e1b468cecb36cf6e1263ec4ef2bc58a447c43
banana0324 <david0324t@gmail.com>
2024-08-26 17:56:34 +0800

    Simon-Game

---
+++ WEB_learning/Simon Game Challenge Starting Files/Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,15 @@
+var gamePartten = [];
+var buttonColours = ["red","blue","green","yellow"];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePartten.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio('./sounds/'+randomChosenColour + '.mp3');
+    audio.play(); 
+}
+
+
