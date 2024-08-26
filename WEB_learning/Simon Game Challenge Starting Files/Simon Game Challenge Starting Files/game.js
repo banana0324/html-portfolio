d17e04f8629bcc6f245d4db20f06915cf0515d80
HAO <david0324t@gmail.com>
2024-08-27 00:12:30 +0800

    Simon-Game

--- WEB_learning/Simon Game Challenge Starting Files/Simon Game Challenge Starting Files/game.js
+++ WEB_learning/Simon Game Challenge Starting Files/Simon Game Challenge Starting Files/game.js
@@ -1,5 +1,13 @@
 var gamePartten = [];
 var buttonColours = ["red","blue","green","yellow"];
+var userClickedPattern = [];
+
+$(".btn").click(function () {
+   var userChosenColour = $(this).attr("id");
+   userClickedPattern.push(userChosenColour);
+   playSound(userChosenColour);
+   animatePress(userClickedPattern)
+});
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
@@ -8,8 +16,17 @@ function nextSequence() {
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio('./sounds/'+randomChosenColour + '.mp3');
+    playSound(randomChosenColour);
+}
+ 
+function playSound(name) {
+    var audio = new Audio('./sounds/'+name + '.mp3');
     audio.play(); 
 }
 
-
+function animatePress(currentColour) {
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("#"+currentColour).removeClass("pressed");
+    },100)
+}
