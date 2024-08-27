843af306b9f71130ef618deb068f66ecc547893e
banana0324 <david0324t@gmail.com>
2024-08-27 17:13:48 +0800

    NPM

---
+++ WEB_learning/backend/2.3 NPM/index.js
@@ -0,0 +1,8 @@
+// import generateName from "sillyname";
+// var sillyName = generateName();
+// console.log(`I am ${sillyName}`);
+
+import {randomSuperhero} from 'superheroes'; 
+const name = randomSuperhero();;
+// console.log(`I am ${name}`);
+console.log(`I am ${name}`);
