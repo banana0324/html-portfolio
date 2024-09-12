d43138402fc9b89ecfe26ad55b5a9838321b8d0c
HAO <david0324t@gmail.com>
2024-09-12 16:16:06 +0800

    Travel Tracker

---
+++ WEB_learning/backend/8.3 Travel Tracker/index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user:"postgres",
+  host:"localhost",
+  database:"world",
+  password:"postgresHao234",
+  port:5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+
+  let countries=[];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  //Write your code here. 
+  const countries = await checkVisisted();
+  res.render("index.ejs", { countries: countries, total: countries.length });
+});
+
+app.post("/add", async (req, res) => {
+  const country = req.body["country"];
+
+  try{
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_code) LIKE '%' || $1 || '%' ;",
+      [country.toLowerCase()]
+    );
+
+    if(result.length !== 0){
+      const data = result.rows[0];
+      const countryCode = data.country_code;
+      await db.query("INSERT INTO visited_countries(country_code) VALUES($1)",
+      [countryCode]);
+
+      res.redirect("/");
+    }
+  }catch(err){
+    console.log(err);
+    const countries = await checkVisisted();
+    res.render("index.ejs",{
+      countries : countries,
+      total: countries.length,
+      error:"Country name does not exist, try again."
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
